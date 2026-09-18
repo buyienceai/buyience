0950ee0bcdeb4f720b0b0241bc7420863d68d106
Girish Laathar <girishkr017@gmail.com>
2026-09-18 13:40:46 +0530

    fixed the blog page

--- app/blog/components/BlogArticleHeader.tsx
+++ app/blog/components/BlogArticleHeader.tsx
@@ -22,7 +22,22 @@ export default function BlogArticleHeader({ post }: Props) {
         </Link>
         <span className="blog-article-cat">{post.categoryLabel}</span>
       </div>
-
+      <h1>{post.title}</h1>
+      <p
+        className="blog-article-dek"
+        style={{ width: "100%", maxWidth: "100%", display: "block" }}
+      >
+        {post.excerpt}
+      </p>
+      <BlogMeta
+        authorName={post.authorName}
+        authorInitials={post.authorInitials}
+        authorImage={post.authorImage}
+        dateLabel={formatPublishedDate(post.publishedAt)}
+        dateIso={post.publishedAt}
+        readingTime={post.readingTime}
+        size="md"
+      />
       <div className={`blog-article-cover${coverContain ? " blog-article-cover--contain" : ""}`}>
         <Image
           src={post.coverImage}
@@ -34,18 +49,6 @@ export default function BlogArticleHeader({ post }: Props) {
           priority
         />
       </div>
-
-      <h1>{post.title}</h1>
-      <p className="blog-article-dek">{post.excerpt}</p>
-      <BlogMeta
-        authorName={post.authorName}
-        authorInitials={post.authorInitials}
-        authorImage={post.authorImage}
-        dateLabel={formatPublishedDate(post.publishedAt)}
-        dateIso={post.publishedAt}
-        readingTime={post.readingTime}
-        size="md"
-      />
     </header>
   );
 }
